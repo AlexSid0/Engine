9685efd37b7a23c3b74255da29765d77d7fdd9c0
AlexSid0 <astro_studios@outlook.com>
2024-07-31 14:36:26 +0100

    ReCache

--- build/CMakeFiles/GenerateGLADHeaders.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/GenerateGLADHeaders"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/GenerateGLADHeaders.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
